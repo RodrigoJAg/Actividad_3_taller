06c4cb382084483baf8190aca8fc4d4b42657294
Rodrigo Aguilar <rjaa2003@hotmail.com>
2024-04-18 19:35:56 -0600

    Actividad 4 Taller

--- src/main/java/com/proyecto/esperanza/controllers/userController.java
+++ src/main/java/com/proyecto/esperanza/controllers/userController.java
@@ -3,11 +3,16 @@ package com.proyecto.esperanza.controllers;
 import com.proyecto.esperanza.dto.productosDTO;
 import com.proyecto.esperanza.dto.transaccionesDTO;
 import com.proyecto.esperanza.dto.usuariosDTO;
+import com.proyecto.esperanza.models.usuariosModel;
 import com.proyecto.esperanza.services.usuarioServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 
 @Controller
@@ -33,6 +38,43 @@ public class userController {
     }
 
 
+    @GetMapping("/menu")
+    public String menu() {
+
+        return "menu";
+    }
+
+    @PostMapping ("/login")
+    public String login(@ModelAttribute("usuario") usuariosDTO usuarioDTO){
+        List<usuariosModel> users = servicioUsuario.getUsers();
+        for(int i = 0; i < users.size(); i++){
+            if(users.get(i).getUsername().equals(usuarioDTO.getUsername()) &&
+                    users.get(i).getPassword().equals(usuarioDTO.getPassword())){
+                return "menu";
+            }
+        }
+        return "index";
+    }
+
+
+    @GetMapping("/reg")
+    public String reg() {
+
+        return "register";
+    }
+
+    @PostMapping ("/register")
+    public String register(@ModelAttribute("usuario") usuariosDTO usuarioDTO){
+        servicioUsuario.saveUser(usuarioDTO);
+        return "menu";
+    }
+
+
+    @GetMapping("/logout")
+    public String logout() {
+
+        return "index";
+    }
     public boolean prueba(String username){
         usuariosDTO user = new usuariosDTO("rodriaac", "123");
         boolean test = false;
